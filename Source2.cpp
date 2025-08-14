5de35875f22e2f535adaa3ba03d98d5c23193b5d
rainybus <rainybus@naver.com>
2025-08-14 11:14:35 +1000

    Update Source2.cpp

--- Source2.cpp
+++ Source2.cpp
@@ -2,29 +2,31 @@
 #include <vector>
 #include <future>    // async, future, promise
 #include <thread>    // hardware_concurrency
+#include <iostream>
+using namespace std;
 
 //соединение подпотоков
 template<typename Iter>
 void merge(Iter begin, Iter mid, Iter end) {
-    std::inplace_merge(begin, mid, end);
+    inplace_merge(begin, mid, end);
 }
 
 template<typename Iter>
 void parallel_merge_sort(Iter begin, Iter end, unsigned int max_threads) {
-    long long len = std::distance(begin, end);
+    long long len = distance(begin, end);
 
     // порог, исходя из возможностей системы
     if (len <= 1024) {
-        std::sort(begin, end);
+        sort(begin, end);
         return;
     }
 
-    Iter mid = std::next(begin, len / 2);
+    Iter mid = next(begin, len / 2);
 
     // параллельный рекурсивный вызов потоков(по возможности)
     if (max_threads > 1) {
         // async - запуск разделенных лев. и правого
-        std::future<void> future_left = std::async(std::launch::async,
+        future<void> future_left = async(launch::async,
             parallel_merge_sort<Iter>,
             begin, mid, max_threads / 2);
 
@@ -44,31 +46,32 @@ void parallel_merge_sort(Iter begin, Iter end, unsigned int max_threads) {
     merge(begin, mid, end);
 }
 
-#include <iostream>
+
 
 int main() {
-    std::vector<int> data = { 38, 27, 43, 3, 9, 82, 10, 1, 5, 2, 7, 6, 4, 8, 0, 11 };
+    vector<int> data = { 38, 27, 43, 3, 9, 82, 10, 1, 5, 2, 7, 6, 4, 8, 0, 11 };
 
     // оценка кол-ва возможных потоков (hardware concurrency)
-    unsigned int num_threads = std::thread::hardware_concurrency();
+    unsigned int num_threads = thread::hardware_concurrency();
     if (num_threads == 0) { // 0 - неясность
         num_threads = 2;
     }
 
     //int num_threads = 2;
-    std::cout << "Original array: ";
+    cout << "Original array: ";
     for (int x : data) {
-        std::cout << x << " ";
+        cout << x << " ";
     }
     std::cout << std::endl;
 
     parallel_merge_sort(data.begin(), data.end(), num_threads);
 
-    std::cout << "Sorted array: ";
+    cout << "Sorted array: ";
     for (int x : data) {
-        std::cout << x << " ";
+        cout << x << " ";
     }
-    std::cout << std::endl;
+    cout << endl;
 
     return 0;
 }
+
